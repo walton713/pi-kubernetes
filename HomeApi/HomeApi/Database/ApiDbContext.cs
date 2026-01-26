62fda4974295419a663e4ec798aabf618951be1f
walton713 <31827896+walton713@users.noreply.github.com>
2026-01-26 05:15:25 -0600

    Build Kubernetes Nodes (#3)
    * ripping everything out
    
    * update readme
    
    * feat: use ansible to build raspberry pi kubernetes nodes
    
    * chore: coderabbit suggestions

--- HomeApi/HomeApi/Database/ApiDbContext.cs
+++
@@ -1,9 +0,0 @@
-using HomeApi.Database.Entities;
-using Microsoft.EntityFrameworkCore;
-
-namespace HomeApi.Database;
-
-public class ApiDbContext(DbContextOptions<ApiDbContext> options) : DbContext(options)
-{
-    public DbSet<LocalWeatherObservation> LocalWeatherObservations { get; set; }
-}
